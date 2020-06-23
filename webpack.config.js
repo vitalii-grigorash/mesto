0b37950cbed4efe53201654bc31e09712a3f5cd1
Vitalii Grigorash <vitalii.grigorash@yandex.ru>
2020-06-23 15:56:08 +0300

    Можно проверять

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: '/node_modules/'
+                exclude: '/node_modules/',
+                options: {
+                    plugins: ['transform-class-properties']
+                }
             },
 
             {
